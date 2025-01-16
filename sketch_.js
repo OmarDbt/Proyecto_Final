c8fcc6d9b8e86ee3c404ae5efc6ca4831db8067f
OMAR BARRETO <omar.barreto@ucuenca.edu.ec>
2025-01-16 14:42:46 -0500

    Actualización  proyecto

---
+++ sketch_.js
@@ -0,0 +1,59 @@
+let video;
+let handPose;
+let hands = [];
+let painting;
+let px = 0;
+let py = 0;
+
+function preload() {
+  handPose = ml5.handPose();
+}
+
+function mousePressed() {
+  console.log(hands);
+  setTimeout(() => save("emitter.png"), 2000);
+
+}
+
+function gotHands(results) {
+  hands = results;
+}
+
+function setup() {
+  createCanvas(640, 480);
+  painting = createGraphics(640, 480);
+  painting.clear();
+
+  video = createCapture(VIDEO);
+  video.hide();
+  handPose.detectStart(video, gotHands);
+
+  let clearButton = createButton("Borrar Lienzo");
+  clearButton.position(550, 540);
+  clearButton.mousePressed(clearCanvas);
+}
+
+function clearCanvas(){
+  painting.clear();
+}
+
+function draw() {
+  image(video, 0, 0);
+  if (hands.length > 0) {
+    let hand = hands[0];
+    let index = hand.index_finger_tip;
+    let thumb = hand.thumb_tip;
+    let x = (index.x + thumb.x) * 0.5;
+    let y = (index.y + thumb.y) * 0.5;
+    
+    let d = dist(index.x, index.y, thumb.x, thumb.y);
+    if (d < 20) {
+      painting.stroke(255, 255, 0);
+      painting.strokeWeight(8);
+      painting.line(px, py, x, y);
+    }
+    px = x;
+    py = y;
+  }
+  image(painting, 0, 0);
+}
